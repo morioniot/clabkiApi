758f2249d59e4dbd8a52bf2f7c17b949cc447a55
Nicolás Fernández <fercholas2012@gmail.com>
2017-06-22 14:18:11 -0500

    Changing Database technology from Postgres to mySQL

--- server.js
+++ server.js
@@ -6,23 +6,31 @@ const express    = require('express');
 //Middleware
 const bodyParser = require('body-parser');
 
-//PostgresSQL
-const massive          = require('massive');
+//MySQL
+const mysql            = require('mysql');
 const connectionInfo   = {
   host: config.postgres.host,
-  port: 5432,
-  database: config.postgres.db,
   user: config.postgres.user,
-  password: config.postgres.password
+  password: config.postgres.password,
+  database: config.postgres.db
 }
-var db;
+
+var connection = mysql.createConnection(connectionInfo); 
+
+connection.connect(function(err){
+	if(!err){
+		console.log("Database is connected");
+	}
+	else{
+		console.log("Error connecting database");
+	}
+})
 
 
 const app = express();
 
 var startExpress = function() {
 	app.listen(config.express.port);
-	db = app.get('db');
 	console.log('API server online')
 	console.log('Listening on port ' + config.express.port);
 };
@@ -30,36 +38,32 @@ var startExpress = function() {
 
 var reportAsLost = function(request, res, next){
 	const major  = request.query.major;
-	const minor  = request.query.minor;
-	const query  = {major: major, minor: minor};
-	const updatedDoc = {reported_as_lost: true}; 
-	db.status.update(query, updatedDoc)
-		.then(lostPet => {
-			console.log(lostPet)
-			if(lostPet.length > 0)
-				res.json(lostPet);
-			else
-				res.json({error:"Major and minor combination does not exist"})
-		})
-		.catch(err => {
-			console.log(err)
-			res.send(500, {error: err.detail})
-		})
+	const minor  = request.query.minor; 
+	connection.query('UPDATE status SET reported_as_lost = ?  WHERE major = ? AND minor = ? AND reported_as_lost = ?', [true, major, minor, false], function (error, results, fields) {
+	  if (error) throw error;
+	  if(results.changedRows > 0){
+	  	 res.json({major:major, minor: minor, reported_as_lost: true});
+	  }
+	  else{
+	  	res.json({error:"Major and minor combination does not exist or reported_as_lost property is already TRUE"})
+	  }
+	});
 };
 
 var register = function(request, res, next){
 	const major  = request.query.major;
 	const minor  = request.query.minor;
 	const newDoc = {major: major, minor: minor, reported_as_lost: false}; 
-	db.status.save(newDoc)
-		.then(register => {
-			console.log(register)
-			res.json(register);
-		})
-		.catch(err => {
-			console.log(err)
-			res.send(500, {error: err.detail})
-		})
+	connection.query('INSERT INTO status SET ?', newDoc, function(err,result){
+	  if(err){
+		res.send(500, {error: err})
+	  }
+	  else{
+		  const savedRegister = {id: result.insertId, major: major, minor: minor, reported_as_lost: false};
+		  console.log(savedRegister);
+		  res.json(savedRegister);	
+	  }
+	});
 };
 
 var getStatus = function(request, res, next){
@@ -67,42 +71,43 @@ var getStatus = function(request, res, next){
 	const minor = request.query.minor;
 	console.log("Major: " + major + " Minor: " + minor);
 	const query = {major: major, minor: minor};
-	db.status.findOne(query)
-		.then( status => {
-			console.log(status);
-			if(status != undefined)
-				res.json(status);
-			else
-				res.json({error:"Major and minor combination does not exist"})
-		})
+	connection.query('SELECT * FROM status WHERE  major ='+ query.major +' AND minor = ' + query.minor + '',function(err,rows){
+		if(err)
+			throw err;
+		if(rows.length > 0){
+			console.log(rows);
+			res.json(rows);	
+		}
+		else{
+			res.json({error:"Major and minor combination does not exist"})
+		}
+	});
 };
 
 var cleanDataBase = function(request, res, next){
-	db.status.destroy({})	
-		.then(destroyedElements => {
-			console.log(destroyedElements)
-			res.json(destroyedElements);
-		})
+	connection.query('DELETE  FROM status', function(err, result){
+    	if (err) throw err;
+    	console.log('Deleted ' + result.affectedRows + ' rows');	
+    	connection.query('ALTER TABLE status AUTO_INCREMENT = 1',function(err){
+    		if(err)
+    			throw err;
+    	})	
+	});
 };
 
 
-//Massive promise
-massive(connectionInfo)
-	.then(instance => {
-	  	app.set('db', instance);
+/// Data Parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extender:true}));
 
-		/// Data Parsing
-		app.use(bodyParser.json());
-		app.use(bodyParser.urlencoded({extender:true}));
+/// Define API routes
+app.route('/api/reportPetAsLost').get(reportAsLost);
+app.route('/api/register').get(register);
+app.route('/api/getStatus').get(getStatus);
+app.route('/api/cleanDataBase').get(cleanDataBase);
 
-		/// Define API routes
-		app.route('/api/reportPetAsLost').get(reportAsLost);
-		app.route('/api/register').get(register);
-		app.route('/api/getStatus').get(getStatus);
-		app.route('/api/cleanDataBase').get(cleanDataBase);
+startExpress();
 
-	    startExpress();
-	});
 
 
 
