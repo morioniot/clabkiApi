98a61f6ad1d0b87754fb88e52561fe40336e2f04
Nicolás Fernández <fercholas2012@gmail.com>
2017-06-22 16:00:33 -0500

    Adding 'Enviroment Variables' support

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const bodyParser = require('body-parser');
 //MySQL
 const mysql            = require('mysql');
 const connectionInfo   = {
-  host: config.postgres.host,
-  user: config.postgres.user,
-  password: config.postgres.password,
-  database: config.postgres.db
+  host: ( process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost'),
+  user: ( process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root'),
+  password: (process.env.OPENSHIFT_MYSQL_DB_PASSWORD || 'p61404'),
+  database: (process.env.OPENSHIFT_GEAR_NAME || 'clabkidb'),
 }
 
 var connection = mysql.createConnection(connectionInfo); 
@@ -30,9 +30,11 @@ connection.connect(function(err){
 const app = express();
 
 var startExpress = function() {
-	app.listen(config.express.port);
-	console.log('API server online')
-	console.log('Listening on port ' + config.express.port);
+	app.set("port", (process.env.OPENSHIFT_NODEJS_PORT || 5000));
+	app.set("ip", (process.env.OPENSHIFT_NODE_IP || "localhost"))	
+	app.listen(app.get("port"), app.get("ip"), function(){
+		console.log("Server started: " + app.get("ip") + ":" + app.get("port") + "/");
+	});
 };
 
 
@@ -40,7 +42,8 @@ var reportAsLost = function(request, res, next){
 	const major  = request.query.major;
 	const minor  = request.query.minor; 
 	connection.query('UPDATE status SET reported_as_lost = ?  WHERE major = ? AND minor = ? AND reported_as_lost = ?', [true, major, minor, false], function (error, results, fields) {
-	  if (error) throw error;
+	  if (error) 
+	  	res.status(500).send({error: err});;
 	  if(results.changedRows > 0){
 	  	 res.json({major:major, minor: minor, reported_as_lost: true});
 	  }
@@ -56,7 +59,7 @@ var register = function(request, res, next){
 	const newDoc = {major: major, minor: minor, reported_as_lost: false}; 
 	connection.query('INSERT INTO status SET ?', newDoc, function(err,result){
 	  if(err){
-		res.send(500, {error: err})
+		res.status(500).send({error: err});
 	  }
 	  else{
 		  const savedRegister = {id: result.insertId, major: major, minor: minor, reported_as_lost: false};
@@ -73,7 +76,7 @@ var getStatus = function(request, res, next){
 	const query = {major: major, minor: minor};
 	connection.query('SELECT * FROM status WHERE  major ='+ query.major +' AND minor = ' + query.minor + '',function(err,rows){
 		if(err)
-			throw err;
+			res.status(500).send({error: err});
 		if(rows.length > 0){
 			console.log(rows);
 			res.json(rows);	
@@ -91,6 +94,7 @@ var cleanDataBase = function(request, res, next){
     	connection.query('ALTER TABLE status AUTO_INCREMENT = 1',function(err){
     		if(err)
     			throw err;
+
     	})	
 	});
 };
